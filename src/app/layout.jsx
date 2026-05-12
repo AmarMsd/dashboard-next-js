5d0173369f5d2ee9ec872449032db5d9b64c5443
Mars <amarmsd1212@gmail.com>
2026-05-12 14:01:52 +0700

    feat: add google fonts

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,9 +1,19 @@
-import "../styles/global.css";
+import "../styles/global.css"; 
+import { Roboto } from "next/font/google";
+
+const roboto = Roboto({
+  weight: ['100', '300', '400', '500', '700', '900'], 
+  style: ['normal', 'italic'],
+  subsets: ['latin'],
+  display: 'swap',
+});
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={roboto.className}>
+        {children}
+      </body>
     </html>
   );
 }
