0123e058fab5910e31c1b596a8dc0c3b665b6483
Mars <amarmsd1212@gmail.com>
2026-05-12 15:57:10 +0700

    feat: add navbar components

--- src/app/(dashboard)/layout.jsx
+++ src/app/(dashboard)/layout.jsx
@@ -1,16 +1,22 @@
+'use client';
+import { useAuth } from '@/hooks/useAuth';
 import SidebarMenu from "../../components/SidebarMenu/SidebarMenu";
 
 export default function DashboardLayout({ children }) {
+  useAuth(); 
+  
   return (
-    <section className="flex min-h-screen">
-      <aside className="w-64 bg-gray-800 text-white">
-        <SidebarMenu />
-      </aside>
+    <section className="flex flex-col min-h-screen">      
+      <div className="flex flex-1">
+        <aside className="w-64 bg-gray-800 text-white">
+          <SidebarMenu />
+        </aside>
 
-      
-      <main className="flex-1 p-8 w-full">
-        {children} {}
-      </main>
+        
+        <main className="flex-1 p-8 w-full bg-gray-50 overflow-auto">
+          {children} {}
+        </main>
+      </div>
     </section>
   );
 }
