12ddcc4b5a8b885ef2c943daad76bec06dd688ef
Mars <amarmsd1212@gmail.com>
2026-05-12 15:56:30 +0700

    feat: add style

--- src/components/InputField/InputField.jsx
+++ src/components/InputField/InputField.jsx
@@ -1,7 +1,7 @@
 export default function InputField({ label, type, value, onChange, placeholder, required = true }) {
   return (
     <div>
-      <label className="block text-sm font-bold text-gray-700 mb-1">
+      <label className="block text-sm font-bold text-white mb-2">
         {label}
       </label>
       <input
@@ -10,7 +10,7 @@ export default function InputField({ label, type, value, onChange, placeholder, 
         onChange={onChange}
         placeholder={placeholder}
         required={required}
-        className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none transition"
+        className="w-full px-4 py-3 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500 outline-none transition text-white-300 bg-[#1e1f1f]"
       />
     </div>
   );
