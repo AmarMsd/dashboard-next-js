5a2ae0bc903df0553c1395b9aa7a49f10e4ee7d7
Mars <amarmsd1212@gmail.com>
2026-05-12 16:40:44 +0700

    fix: remove user management

--- src/app/(dashboard)/userManagement/create/page.jsx
+++
@@ -1,8 +0,0 @@
-export default function CreateUserPage() {
-  return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">Create User Page</h1>
-      <p className="text-gray-500 mt-2">Halaman ini sedang dalam tahap pengembangan.</p>
-    </div>
-  );
-}
